4f0eab42263360680b8edae5ae267d4f5f1728ad
Mahounda <roromahounda@gmail.com>
2025-11-22 12:00:06 +1000

    users authentication

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -7,11 +7,11 @@ class Fundraiser(models.Model):
   image = models.URLField()
   is_open = models.BooleanField()
   date_created = models.DateTimeField(auto_now_add=True)
-owner = models.ForeignKey(
-  get_user_model(),
-  on_delete=models.CASCADE,
-  related_name='owned_fundraisers'
-)
+  owner = models.ForeignKey(
+    get_user_model(),
+    on_delete=models.CASCADE,
+    related_name='owned_fundraisers'
+  )
 
 class Pledge(models.Model):
   amount = models.IntegerField()
