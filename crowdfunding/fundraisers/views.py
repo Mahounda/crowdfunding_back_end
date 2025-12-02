2f7301449edc0f82ef3722be4dfef30b8f4fe462
Mahounda <roromahounda@gmail.com>
2025-12-02 18:46:00 +1000

    fundraising and pledges creation and test

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -16,12 +16,6 @@ class FundraiserList(APIView):
         IsOwnerOrReadOnly
     ]
 
-class FundraiserList(APIView):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsSupporterOrReadOnly
-    ]
-
     def get(self, request):
         fundraisers = Fundraiser.objects.all()
         serializer = FundraiserSerializer(fundraisers, many=True)
@@ -84,6 +78,43 @@ class PledgeList(APIView):
                 serializer.data,
                 status=status.HTTP_201_CREATED
             )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class PledgeDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsSupporterOrReadOnly
+    ]
+
+    def get_object(self, pk):
+        try:
+            print(pk)
+            pledge = Pledge.objects.get(pk=pk)
+            print(pledge)
+            self.check_object_permissions(self.request, pledge)
+            return pledge
+        except Pledge.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeSerializer(pledge)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeSerializer(
+        instance=pledge,
+        data=request.data,
+        partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
