91eeaa9c3d52b1ed14fda5d130b25e99263c37b0
Mahounda Poinsonnet <roromahounda@gmail.com>
2026-02-09 00:25:45 +1000

    Fix permission serializer

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -31,6 +31,5 @@ class FundraiserDetailSerializer(FundraiserSerializer):
         instance.goal = validated_data.get('goal', instance.goal)
         instance.is_open = validated_data.get('is_open', instance.is_open)
         instance.date_created = validated_data.get('date_created', instance.date_created)
-        instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
